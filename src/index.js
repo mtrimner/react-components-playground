f5ee7ee92d21d5920a119d99e22e69ef9edad2ce
mtrimner <matt.trimner@gmail.com>
2020-12-31 13:50:26 -0800

    first commit and experiemental code added to learn react component system.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" image={faker.image.image()} commentText="Great!"/>
+           </ApprovalCard>
+
+           <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 3:23PM" image={faker.image.image()} commentText="Wow!"/>
+           </ApprovalCard>
+
+           <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 9:45PM" image={faker.image.image()} commentText="Interesting!"/>
+           </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
